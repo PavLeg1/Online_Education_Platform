625956bd8b7a89423796fa4e0472c015440fbbca
PavLeg1 <pavlego2000@yandex.ru>
2023-11-01 13:33:24 +0300

    Отображение курсов в корневом разделе "/"

--- educa/urls.py
+++ educa/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
+from courses.views import CourseListView
 
 
 urlpatterns = [
@@ -13,6 +14,8 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     
     path('course/', include('courses.urls')),
+
+    path('', CourseListView.as_view(), name='course_list'),
 ]
 
 # Добавили функцию static чтобы раздавать файлы во время разработки встроенным сервером разработки
